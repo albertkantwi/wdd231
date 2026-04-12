f2b25c7754b321dbbda43bc89de7c7e54b4c9011
Albert <albertkantwi@gmail.com>
2026-04-12 10:20:33 +0000

    Add images

--- chamber/data/attractions.mjs
+++ chamber/data/attractions.mjs
@@ -7,49 +7,49 @@ export const jsonData = {
             "address": "6550 Gateway Road",
             "description" : "The Rocky Mountain Arsenal was a Cold War era chemical weapons manufacturing site which has been transformed into a large urban wildlife refuge hosting over 330 species.",
             "website": "https://www.fws.gov/refuge/rocky-mountain-arsenal",
-            "photo" : "images/arsenal.webp"
+            "photo" : "images/bike.jpg"
         },
         {
             "name": "Sand Creek Regional Greenway", 
             "address": "4900 Sandcreek Dr. S", 
             "description": "A nearly 13 mile, mostly paved public trail connecting Commerce City, Denver, and Aurora.  Running from the High Line Canal in Aurora to the South Platte River in Commerce City, the Sand Creek Regional Greenway provides a native ecosystem for walking, biking, and wildlife viewing.",
             "website": "https://www.sandcreekgreenway.org/",
-            "photo": "images/sandCreek.webp"
+            "photo": "images/canue.jpg"
         },
         {
             "name" : "Historic Derby",
             "address": "72nd and Monaco",
             "description" : "A 1950's era commercial hub, featuring well-preserved structures from that period.",
             "website": "https://www.c3gov.com/Government/Urban-Renewal/Derby-Downtown-District",
-            "photo": "images/derby.webp"
+            "photo": "images/independence.jpg"
         },
         {
             "name": "Commerce City Museum",
             "address": "6505 East 60th Avenue", 
             "description": "A museum preserving the rich history of Commerce City since it's settling in 1850.", 
             "website": "https://www.cchistoric.com/about-the-museum",
-            "photo": "images/museum.webp"
+            "photo": "images/jamestown.jpg"
         },
         {
             "name": "Buffalo Run Golf Course", 
             "address": "15700 East 112th Avenue", 
             "description": "An 18-hole, par-72 municipal Golf course designed by Keith Foster.  The course has 65 bunkers, challenging wind conditions, and lengths from 5,277 to 7,411 yards while offering Rocky Mountain Views.", 
             "website": "https://www.buffalorungolfcourse.com/",
-            "photo": "images/buffaloRun.webp"
+            "photo": "images/kente.jpg"
         },
         {
             "name": "Paradise Island at Pioneer Park",
             "address": "5951 Monaco", 
             "description": "State-of-the-art 2 acre outdoor water park with a 5,000 square foot zero-depth leisure pool, three water slides, a 250 foot lazy river, a 1,700 square foot toddler pool with features, and an activities pool with lap lanes.", 
             "website": "https://www.c3gov.com/Parks-Recreation-Golf/Pools/Paradice-Island-Pool-at-Pioneer-Park",
-            "photo": "images/pioneer-park.webp"
+            "photo": "images/sea.jpg"
         },
         {
             "name": "88 DriveIn Theater", 
             "address": "8780 Rosemary Street", 
             "description": "The last remaining original drive-in theater in the Denver Metropolis in operation since 1972.  Drive on in nightly for at least a double feature starting at dusk spring through fall.", 
             "website": "https://www.88drivein.net/",
-            "photo": "images/88DriveIn.webp"
+            "photo": "images/trotro.jpg"
         },
         {
             "name": "Mile High Flea Market", 
