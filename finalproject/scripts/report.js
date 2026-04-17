f18393013a8354c6aa35fb19826917e82719c8a0
Albert <albertkantwi@gmail.com>
2026-04-17 18:03:09 +0000

    Add report page

---
+++ finalproject/scripts/report.js
@@ -0,0 +1,10 @@
+import { populateFooter } from "./footer.js";
+import { setupNav } from "./navigation.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    setupNav(); 
+    populateFooter()
+
+
+})
